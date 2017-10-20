3303429f128780f74d58354071aa725ec6d87c4a
m <chenhui315503490@126.com>
2017-10-20 16:29:03 +0800

    shop 页面

--- src/app/shopping-cart/shopping-cart-routing.module.ts
+++ src/app/shopping-cart/shopping-cart-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 import {IndexComponent} from './index/index.component';
+import {SubmitComponent} from './submit/submit.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'index'},
-  {path: 'index', component: IndexComponent}
+  {path: 'index', component: IndexComponent},
+  {path: 'submit', component: SubmitComponent},
 ];
 
 @NgModule({
--- src/app/shopping-cart/shopping-cart.module.ts
+++ src/app/shopping-cart/shopping-cart.module.ts
@@ -2,14 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {IndexComponent} from './index/index.component';
 import {ShoppingCartRoutingModule} from './shopping-cart-routing.module';
-import {ShareModule} from '../share/share.module'
+import {ShareModule} from '../share/share.module';
+import { SubmitComponent } from './submit/submit.component'
 @NgModule({
   imports: [
     CommonModule,
     ShoppingCartRoutingModule,
     ShareModule
   ],
-  declarations: [IndexComponent]
+  declarations: [IndexComponent, SubmitComponent]
 })
 export class ShoppingCartModule {
 }
