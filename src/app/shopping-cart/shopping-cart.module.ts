008bcfb911ae80d51f668363679d7bb30760569f
m <chenhui315503490@126.com>
2017-10-20 11:37:36 +0800

    shop 页面

--- src/app/shopping-cart/shopping-cart.module.ts
+++ src/app/shopping-cart/shopping-cart.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {IndexComponent} from './index/index.component';
 import {ShoppingCartRoutingModule} from './shopping-cart-routing.module';
+import {ShareModule} from '../share/share.module'
 @NgModule({
   imports: [
     CommonModule,
-    ShoppingCartRoutingModule
+    ShoppingCartRoutingModule,
+    ShareModule
   ],
   declarations: [IndexComponent]
 })
