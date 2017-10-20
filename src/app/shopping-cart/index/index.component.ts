568726b8ae90ba9133a41fb60b52d1b114361b00
m <chenhui315503490@126.com>
2017-10-20 16:46:29 +0800

    shop 页面

--- src/app/shopping-cart/index/index.component.ts
+++ src/app/shopping-cart/index/index.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  goodsNumbers = 1;
+  showNum: boolean = false;
+  changeNumStatus: string = '编辑';
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  /**
+   * 加数量
+   */
+  goodsNumPlus(): void {
+    this.goodsNumbers += 1;
+  }
+
+  /**
+   * 减数量
+   */
+  goodsNumMinus(): void {
+    if (this.goodsNumbers === 1
+    ) {
+      return;
+    }
+    this.goodsNumbers -= 1;
+  }
+
+  changeNum(): void {
+    if (!this.showNum) {
+      this.showNum = true;
+      this.changeNumStatus = '完成';
+    } else {
+      this.showNum = false;
+      this.changeNumStatus = '编辑';
+    }
+  }
+
+  changeSubmit(): void {
+    this.router.navigate(['/shoppingCart/submit']);
+  }
 }
