d29e32d61d615f374299734aeb49a431126e91cc
mr.chen <chenhui315503490@126.com>
2017-10-23 10:29:57 +0800

    添加路由

--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -3,11 +3,17 @@ import {Routes, RouterModule} from '@angular/router';
 import {OrdersConfirmComponent} from './orders-confirm/orders-confirm.component';
 import {OrdersPaymentComponent} from './orders-payment/orders-payment.component';
 import {OrdersSubmitComponent} from './orders-submit/orders-submit.component';
+import {OrdersPendingPayComponent} from './orders-pending-pay/orders-pending-pay.component';
+import {OrdersPendingReceiveComponent} from './orders-pending-receive/orders-pending-receive.component';
+import {OrdersPendingDeliverComponent} from './orders-pending-deliver/orders-pending-deliver.component';
 
 const routes: Routes = [
   {path: 'confirm', component: OrdersConfirmComponent},
   {path: 'payment', component: OrdersPaymentComponent},
   {path: 'submit', component: OrdersSubmitComponent},
+  {path: 'pendingPay', component: OrdersPendingPayComponent},
+  {path: 'pendingReceive', component: OrdersPendingReceiveComponent},
+  {path: 'pendingDeliver', component: OrdersPendingDeliverComponent},
 ];
 
 @NgModule({
