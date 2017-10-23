25fcd8881bb34629aec6900c8c8cf284b8c37350
m <chenhui315503490@126.com>
2017-10-23 13:43:32 +0800

    修改 收藏 tab

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,18 +3,15 @@ import {CommonModule} from '@angular/common';
 import {IndexComponent} from './index/index.component';
 import {UserRoutingModule} from './user-routing.module';
 import {ShareModule} from '../share/share.module';
-import { CollectComponent } from './collect/collect.component';
-import { AddressComponent } from './address/address.component';
-import { GoodsComponent } from './collect/goods/goods.component';
-import { ContentsComponent } from './collect/contents/contents.component';
-
+import {CollectComponent} from './collect/collect.component';
+import {AddressComponent} from './address/address.component';
 @NgModule({
   imports: [
     CommonModule,
     UserRoutingModule,
     ShareModule
   ],
-  declarations: [IndexComponent, CollectComponent, AddressComponent, GoodsComponent, ContentsComponent]
+  declarations: [IndexComponent, CollectComponent, AddressComponent]
 })
 export class UserModule {
 }
